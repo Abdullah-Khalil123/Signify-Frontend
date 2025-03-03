e030c6388c99da0b6fc299c24e2f34315e012b4e
M-Ali-Haider <mah01134212100@gmail.com>
2025-03-03 23:12:35 +0500

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import {
+  Poppins_400Regular,
+  Poppins_500Medium,
+} from "@expo-google-fonts/poppins";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import "../global.css";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +22,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Poppins_400Regular,
+    Poppins_500Medium,
   });
 
   useEffect(() => {
@@ -28,9 +37,10 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
---
+++ app/index.tsx
@@ -0,0 +1,69 @@
+import GoogleLogoSVG from "@/assets/Google";
+import { ResizingButton } from "@/components/Buttons/resizable";
+import InputField1 from "@/components/InputFields/InputField1";
+import LayoutWrapper from "@/components/LayoutWrapper";
+import Checkbox from "expo-checkbox";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
+
+export default function App() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isChecked, setChecked] = useState(false);
+
+  return (
+    <LayoutWrapper>
+      <View className="bg-white h-full items-center justify-center px-4">
+        <View className="w-full">
+          <Text className="text-center font-poppins500 text-lg leading-[27px] mb-8">
+            Login to proceed
+          </Text>
+          <InputField1
+            placeholder={"Email"}
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+          />
+          <InputField1
+            placeholder={"Password"}
+            value={password}
+            onChangeText={(text) => setPassword(text)}
+            className="mt-6"
+          />
+          <Pressable
+            onPress={() => setChecked(!isChecked)}
+            className="my-8 flex-row items-center"
+          >
+            <Checkbox
+              value={isChecked}
+              // onValueChange={setChecked}
+              color={"#F46A62"}
+              style={{ width: 18, height: 18, borderRadius: 5 }}
+            />
+            <Text className="font-poppins500 ml-2.5 text-xs">Remember me</Text>
+          </Pressable>
+
+          <ResizingButton
+            title="Sign in"
+            onPress={() => {}}
+            className="bg-[#F46A62] py-4"
+            textStyles="text-base text-white"
+            isLoading={false}
+            isDisabled={false}
+          />
+          <Text className="text-xs leading-[18px] font-poppins500 my-6 text-center">
+            Or
+          </Text>
+          <ResizingButton
+            title="Google"
+            Icon={GoogleLogoSVG}
+            onPress={() => {}}
+            className="bg-[#FFF6F5] py-4 border border-[#F46A62]"
+            textStyles="text-base text-black"
+            isLoading={false}
+            isDisabled={false}
+          />
+        </View>
+      </View>
+    </LayoutWrapper>
+  );
+}
